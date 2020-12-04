3597eb1c0f900d44bff8c83a1b4e19b4bba0687c
Sebastian Kalucza <sebastian.kalucza@gmail.com>
2020-12-04 09:40:02 +0000

    version 0.2.0

--- R/read_hilda.R
+++ R/read_hilda.R
@@ -13,6 +13,7 @@
 #' @param waves The wave(s) you want the domain variables to come from. Can be left empty if using custom file names.
 #' @param dir Directory where the STATA files are located. Default is the current working directory.
 #' @param filenames Vector of .dta files to be read. Default is names used by the Australian Data Archive.
+#' @param release Release number of the wave to be read. Defaults to 18. (newest wave)
 #'
 #' @author Sara Kalucza & Sebastian Kalucza
 #'
@@ -24,44 +25,36 @@
 #'
 #' @examples
 #' path <- system.file("extdata", package = "hildareadR")
-#' # Reads variable hibiff and all variables named hgage from waves 1 to 3 in directory fdir
-#' read_hilda(c("hibiff", "hgage"), waves = 1:3, dir = path)
-#' # Reads variable aiopeye from custom files in directory fdir
-#' read_hilda("aiopeye", dir = path, filenames = c("custom1.dta", "custom2.dta"))
+#' # Reads variable hibiff and hgage from waves 1 to 3 in directory fdir and from release 17
+#' read_hilda(c("hibiff", "hgage"), waves = 1:3, dir = path, release = 17)
+#' # Reads variable aiopeye from custom files in directory fdir and from release 17
+#' read_hilda("aiopeye", dir = path, filenames = c("custom1.dta", "custom2.dta"), release = 17)
 
-read_hilda <- function(domain, waves = NULL, dir = NULL, filenames = NULL){
-  stopifnot(is.character(domain), is.numeric(waves) | is.null(waves), is.null(dir) | is.character(dir),
-            is.null(filenames) | is.character(filenames))
-  #Check that user has entered either waves or custom filenames and return error if not
-  if(is.null(waves) & is.null(filenames)){
+read_hilda <- function (domain, waves = NULL, dir = NULL, filenames = NULL, release = 18){
+  stopifnot(is.character(domain), is.numeric(waves) | is.null(waves),
+            is.null(dir) | is.character(dir), is.null(filenames) |
+            is.character(filenames))
+  if (is.null(waves) & is.null(filenames)) {
     stop("waves and filenames can't both be empty")
   }
-  # Add .dta filenames into list
-  if(is.null(filenames)) {
-    file_list <- paste0("Combined_", letters[waves], "170c.dta")
-  } else {
+  if (is.null(filenames)) {
+    file_list <- paste0("Combined_", letters[waves], release, "0c.dta")
+  }
+  else {
     file_list <- filenames
   }
-  #Adds file directory to filenames if added by user
-  if(!is.null(dir)){
+  if (!is.null(dir)) {
     file_list <- paste0(dir, "/", file_list)
   }
-  #Add REGEX to domain name to only grab correct variables
   domain <- paste0("^.", domain)
-  #Load first STATA file into temporary df
   df <- read_dta(file = file_list[1])
-  #Subset df with domain and id
-  df <- df[,c(1, grep(paste(domain,collapse = "|"), names(df)))]
-  #Add wave ID
+  df <- df[, c(1, grep(paste(domain, collapse = "|"), names(df)))]
   df$wave <- substring(names(df)[2], 1, 1)
-  #Remove wave letter from beginning each variable name
   names(df)[c(-1, -length(names(df)))] <- substring(names(df)[c(-1, -length(names(df)))], 2)
-  #Remove STATA labels
   df <- zap_labels(df)
-  #Loop to add all waves into one df
-  for(i in file_list[-1]){
+  for (i in file_list[-1]) {
     temp <- read_dta(file = i)
-    temp <- temp[,c(1, grep(paste(domain,collapse = "|"), names(temp)))]
+    temp <- temp[, c(1, grep(paste(domain, collapse = "|"), names(temp)))]
     temp$wave <- substring(names(temp)[2], 1, 1)
     names(temp)[c(-1, -length(names(temp)))] <- substring(names(temp)[c(-1, -length(names(temp)))], 2)
     temp <- zap_labels(temp)
